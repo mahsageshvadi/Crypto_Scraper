bf0046d332a6f1ff38f83146256807084f333dc6
Mahsa Geshvadi <mahsa@Mahsas-MacBook-Air.local>
2023-01-08 23:35:34 -0500

    crawler class gets data

---
+++ reddit_crawler.py
@@ -0,0 +1,63 @@
+
+import requests
+
+
+class SubReddit_crawler:
+
+
+	def set_header(self):
+
+		CLIENT_ID = '-UwFL3ODpmMLGhcq509XFA'
+		SECRET_KEY = 'rwhzEOPD9kqbgxj34JxuS7vcMrMlWg'
+
+		auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+		self.headers = {'User-Agent': 'cryptocurrency 1.0 by /u/mahsageshvadi'}
+		data = {
+	            'grant_type': 'password',
+	            'username': 'mahsageshvadi',
+	            'password': 'Mimoolijoon1'
+	        }
+		res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=self.headers)
+		TOKEN = res.json()
+		Acces_Token = TOKEN['access_token']
+
+		self.headers['Authorization'] = f'bearer {Acces_Token}'
+
+	def __init__(self, subreddit_name):
+
+		self.subreddit_name = subreddit_name
+		self.set_header()
+
+
+
+
+	#Gets new posts, if use Last_post_ID to avoid getting redunant data when crawling 
+
+	def get_new_posts(self, Last_post_ID = None):
+
+		if Last_post_ID is not None:
+
+			params = {'before': Last_post_ID}
+
+		else:
+			params = {}
+
+		new_posts = requests.get('https://oauth.reddit.com/r/{}/new'.format(self.subreddit_name), headers= self.headers,
+                               params= params)
+
+      #  if res_new.status_code != 200:
+      #      self.reset_header()
+      #      self.check_new_data(id)
+		return new_posts
+
+
+reddit_crawler = SubReddit_crawler('CryptoCurrency')
+
+print(reddit_crawler.get_new_posts().json())
+
+
+
+
+
+
